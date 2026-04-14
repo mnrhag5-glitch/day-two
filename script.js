a1132a29a239516789eb5e1c2a6f1d38157d59c4
Gautam Thakur <mnrhag5@gmail.com>
2026-04-14 09:51:22 +0530

    update everything

---
+++ script.js
@@ -0,0 +1,79 @@
+//Hello! today im learning and prcticing javascript,in js we are 
+//learning about variables , operators,data types.
+//so lets start.
+
+
+//variables ---
+
+// Name="gautam";
+// age=22;
+// gender="male"
+// proffession="full stack developer";
+// married="false";
+// console.log(age);
+
+
+//so these are variable ,we can create our variable like any kind of name ,we can add numbers in name(use after or in the middle of the word)
+//we can use syble like doller,minus and some more.
+//there are three type keyword var , let ,const.
+//so lets start it.
+
+
+// var name="gautam";
+// var age=22;
+// var gender="male"
+
+// console.log(gender);
+
+//let keyword
+
+// let name="ram";
+// let age=22;
+// let gender="male"
+// console.log(age);
+
+//data typee-->string,number,boolean,null,undifine,symble,bigint
+//and there are non-primitve--object,fnction,array
+
+//let is block sccope 
+
+let name="gautam";
+name="seju";
+console.log(name);
+
+let info={
+    name:"seju",
+    age:21,
+    gender:"female",
+    from:"badon",
+    merried:"false"
+}
+console.log(info.gender);
+console.log(info.age);
+console.log(info.name);
+console.log(info.merried);
+console.log(info);
+
+
+//so this is object 
+
+//we can change some info under it---
+
+let about={
+    name:"seju",
+    age:21,
+    gender:"female",
+    from:"badon",
+    merried:"false"
+}
+about.age=27;
+about.from="dumehar";
+
+console.log(about);
+
+
+//so this is object and this is how we can check key,value and change the value
+//now next is operators and condition
+
+
+
