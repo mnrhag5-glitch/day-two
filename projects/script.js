cf231823ba8cab041c055311a9335f962482b46b
Gautam Thakur <mnrhag5@gmail.com>
2026-04-29 10:28:10 +0530

    add

--- projects/script.js
+++ projects/script.js
@@ -1,1 +1,19 @@
-console.log("Hello, World!");
+const buttons=document.querySelectorAll('.button');
+const body=document.querySelector('body');
+
+buttons.forEach(function(button){
+    button.addEventListener('click',function(e){
+        if(e.target.id==='pink'){
+            body.style.backgroundColor='pink';
+        }
+        if(e.target.id==='green'){
+            body.style.backgroundColor='green';
+        }
+        if(e.target.id==='blue'){
+            body.style.backgroundColor='blue';
+        }
+        if(e.target.id==='yellow'){
+            body.style.backgroundColor='yellow';
+        }
+    });
+});
